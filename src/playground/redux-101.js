438c1e075e1dde7f404643ea71d8b2ec59d5f8fa
Your Name <you@example.com>
2021-01-16 21:42:14 +0330

    principle of using multiple reducers

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,39 +1,67 @@
-import { createStore } from "redux";
+import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }, action) => {
+// Action generators - functions that return action objects
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const setCount = ({ count }) => ({
+  type: 'SET',
+  count
+});
+
+const resetCount = () => ({
+  type: 'RESET'
+});
+
+// Reducers
+// 1. Reducers are pure functions
+// 2. Never change state or actiton
+
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
-    case "INCREMENT":
-      const incrementBy =
-        typeof action.incrementBy === "number" ? action.incrementBy : 1;
+    case 'INCREMENT':
       return {
-        count: state.count + incrementBy,
+        count: state.count + action.incrementBy
       };
-    case "DECREMENT":
-      const decrementBy =
-        typeof action.decrementBy === "number" ? action.decrementBy : 1;
+    case 'DECREMENT':
       return {
-        count: state.count - decrementBy,
+        count: state.count - action.decrementBy
       };
-    case "SET":
+    case 'SET':
       return {
-        count: action.count,
+        count: action.count
       };
-    case "RESET":
+    case 'RESET':
       return {
-        count: 0,
+        count: 0
       };
     default:
       return state;
   }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({ type: "INCREMENT", incrementBy: 5 });
-store.dispatch({ type: "INCREMENT" });
-store.dispatch({ type: "RESET" });
-store.dispatch({ type: "DECREMENT" });
-store.dispatch({ type: "DECREMENT", decrementBy: 10 });
-store.dispatch({ type: "SET", count: 101 });
+store.dispatch(incrementCount({ incrementBy: 5 }))
+
+store.dispatch(incrementCount());
+
+store.dispatch(resetCount());
+
+store.dispatch(decrementCount());
+
+store.dispatch(decrementCount({ decrementBy: 10 }));
+
+store.dispatch(setCount({ count: -100 }));
--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,37 @@
 import { createStore, combineReducers } from "redux";
 
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const filterReducerDefaultState = {
+  text: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined,
+};
+
+const filterReducer = (state = filterReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filterReducer,
+  })
+);
+
+console.log(store.getState());
+
 const demoState = {
   expenses: [
     {
